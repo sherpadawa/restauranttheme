ae9a8dc61606e7f86ad07ad907c29ba3c8edd343
Dawa Sherpa <dawa.sherpa@mihsnyc.org>
2016-07-28 11:50:16 -0400

    changed fluids, font, color

---
+++ sidebar.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * The sidebar containing the main widget area
+ *
+ * @package _tk
+ */
+?>
+
+	</div><!-- close .main-content-inner -->
+
+	<div class="sidebar col-sm-12 col-md-4">
+
+		<?php // add the class "panel" below here to wrap the sidebar in Bootstrap style ;) ?>
+		<div class="sidebar-padder">
+
+			<?php do_action( 'before_sidebar' ); ?>
+			<?php if ( ! dynamic_sidebar( 'sidebar-1' ) ) : ?>
+
+				</aside>
+
+			<?php endif; ?>
+
+		</div><!-- close .sidebar-padder -->
