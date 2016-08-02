650f506223e644cf0d70b892a8914c8280da476a
Dawa Sherpa <dawa.sherpa@mihsnyc.org>
2016-08-02 14:16:27 -0400

    added an image

--- functions.php
+++ functions.php
@@ -1,9 +1,9 @@
 <?php
 function my_theme_enqueue_styles() {
+$parent_style = 'parent-style';
 
-    $parent_style = 'parent-style';
-
-    wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
+wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
+    
     wp_enqueue_style( 'child-style',
         get_stylesheet_directory_uri() . '/style.css',
         array( $parent_style )
